5c98d1e713c594ccd575af2d0f057865889de088
Ahacad <1393345075@qq.com>
2020-06-30 15:03:11 +0800

    update great

---
+++ Great/useme.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+import os
+
+
+path = "/home/ahacad/Workstation/Prepared/wallpaper/Great/"
+for item in os.listdir(path):
+    if len(item) > 15:
+        name = item.split(".")[1]
+        name = int(name) + 32
+        name = str(name)
+        os.rename(path + item, path + name)
