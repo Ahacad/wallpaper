ac6034ec69b0c446abd1d01c6d2e6719db13029c
Ahacad <1393345075@qq.com>
2020-07-07 19:33:31 +0800

    Update great

--- Great/useme.py
+++ Great/useme.py
@@ -7,6 +7,6 @@ path = "/home/ahacad/Workstation/Prepared/wallpaper/Great/"
 for item in os.listdir(path):
     if len(item) > 15:
         name = item.split(".")[1]
-        name = int(name) + 32
+        name = int(name) + 101
         name = str(name)
         os.rename(path + item, path + name)
